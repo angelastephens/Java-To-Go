9424ea3b765171809f31adfc77e7c42295af8725
angela stephens <angelastephens317@gmail.com>
2020-09-09 21:01:08 -0400

    add back ground image to drink index and show

--- app/controllers/drinks_controller.rb
+++ app/controllers/drinks_controller.rb
@@ -2,6 +2,7 @@ class DrinksController < ApplicationController
 
   # GET: /drinks- done 
   get "/drinks" do
+    @bg = "https://media1.thehungryjpeg.com/thumbs2/ori_3816392_xb548tcv59f8f0sj0rv1ezc3j70zg6794umksinl_coffee-cup-seamless-pattern-sketch-tea-and-coffee-cups-hot-drinks-va.jpg"
     erb :"/drinks/index.html"
   end
 
@@ -21,11 +22,13 @@ class DrinksController < ApplicationController
 
   # GET: /drinks/5
   get "/drinks/:id" do
-    @drinks = Drink.find(params[:id])
+    @bg = "https://images.fineartamerica.com/images/artworkimages/mediumlarge/2/hand-drawn-background-of-electric-coffee-grinder-with-coffee-beans-iam-nee.jpg"
+    @drinks = Drink.find(params[:id])  
     erb :"/drinks/show.html"
   end
 
   # GET: /drinks/5/edit
+    
   get "/drinks/:id/edit" do
     erb :"/drinks/edit.html"
   end
