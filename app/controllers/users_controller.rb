eb532c3558da7c5a3c9dcbfea6532d552e98561d
angela stephens <angelastephens317@gmail.com>
2020-09-15 21:24:08 -0400

    update with commented out notes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,24 +1,24 @@
 class UsersController < ApplicationController
 
 
-  get '/users/new' do 
+  get '/users/new' do  #renders the new user form to sign up / sends the date to post 
 
       erb :'/user/new'
   end 
 
-  post '/signup' do
+  post '/signup' do #checks to make sure the use entered all the required params before creating a new user, if yes then redirects to their new drink page.
     @user = User.new(first_name: params[:first_name],last_name: params[:last_name],birthday: params[:birthday], phone_number: params[:phone_number], email_address: params[:email_address], user_name: params[:user_name], password: params[:password])
     if @user.save
       session[:id] = @user.id
       redirect "/drinks/new"
-    else 
-      @error = "Ooops missing field or user name is take. Please try again!"
+    else # if a param is missing it will redirect the use to sign up again 
+      @error = "Ooops missing field or user name is taken. Please try again!"
       erb :'/user/new'
     end
     
   end
 
-  get '/home' do
+  get '/home' do #renders my homepage 
 
     erb :'/user/homepage'
   end
