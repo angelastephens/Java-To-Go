731154b2ceb11f3a7d6c75856e2f83e2b65f5a0d
angela stephens <angelastephens317@gmail.com>
2020-09-15 21:24:36 -0400

    add validates to drink model

--- app/models/drink.rb
+++ app/models/drink.rb
@@ -1,6 +1,11 @@
 
 class Drink < ActiveRecord::Base 
     belongs_to :user
-
+    validates :drink_name, presence: true
+    validates :temperature, presence: true
+    validates :caffeine_type, presence: true
+    validates :milk_options, presence: true
+    validates :flavors, presence: true
+    
 
 end 
