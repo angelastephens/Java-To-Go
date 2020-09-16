fecd7f1af1e450d2962726d9bec7bca47debda83
angela stephens <angelastephens317@gmail.com>
2020-09-15 21:23:30 -0400

    update sessions controller with background image

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,17 +1,17 @@
 class SessionsController < ApplicationController
-    
-    get '/login' do 
+    #sessions controller handles the begining and end of a session 
+    get '/login' do #renders users log in form 
       @bg = "https://image.shutterstock.com/image-vector/bag-coffee-beans-vintage-style-600w-1468619435.jpg"
       erb :'/user/login'
     end
   
-    post '/login' do 
+    post '/login' do # collect the users log in information and makes sure the username and password is valid then starts a new session with the id 
       @bg = "https://image.shutterstock.com/image-vector/bag-coffee-beans-vintage-style-600w-1468619435.jpg"
 
       # find the user by their user name:
       user = User.find_by(user_name: params[:user_name])
       # if they typed in the right password then log them in, if not show them the form again
-      if user && user.authenticate(params[:password])
+      if user && user.authenticate(params[:password]) #authenticate hashes what the users types in then compares it to the hash version of the original password 
         session[:id] = user.id
         redirect "/home"
       else 
@@ -20,7 +20,7 @@ class SessionsController < ApplicationController
       end
     end
 
-    delete '/logout' do 
+    delete '/logout' do #logs out and clears the session hash 
       session.clear
       redirect "/"
     end
